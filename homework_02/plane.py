6603acf4818f51383057a84e66fe3af45485426a
user <JIK0009@yandex.ru>
2022-03-31 14:45:18 +0300

    class Plane created

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,25 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02 import exceptions
+
+
+class Plane(Vehicle):
+    cargo = 0
+    max_cargo = 0
+
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.max_cargo = max_cargo
+
+    def load_cargo(self, number):
+        if self.cargo + number <= self.max_cargo:
+            self.cargo += number
+        else:
+            raise exceptions.CargoOverload
+
+    def remove_all_cargo(self):
+        removed_cargo = self.cargo
+        self.cargo = 0
+        return removed_cargo
