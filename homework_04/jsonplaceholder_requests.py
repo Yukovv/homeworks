db7c97530baebf2f08d9b883d8a94a1ce2a15846
user <JIK0009@yandex.ru>
2022-06-05 16:38:28 +0300

    update module jsonplaceholder_requests

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -1,6 +1,31 @@
 """
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
+import asyncio
 
-USERS_DATA_URL = ""
-POSTS_DATA_URL = ""
+import aiohttp
+
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
+
+
+async def query_users():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(USERS_DATA_URL) as resp:
+            print(resp.status)
+            print(resp.text)
+
+
+async def query_posts():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(POSTS_DATA_URL) as resp:
+            print(resp.status)
+            print(resp.text)
+
+
+async def fetch_json(url: str) -> list[dict]:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            data: list[dict] = await resp.json()
+
+            return data
