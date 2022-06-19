df4a00e2e07b5b0ca17767214973aef98c1c376a
user <JIK0009@yandex.ru>
2022-06-19 13:24:45 +0300

    create index.html and about.html

--- homework_05/app.py
+++ homework_05/app.py
@@ -18,12 +18,12 @@ app = Flask(__name__)
 
 @app.get("/", endpoint="index")
 def index_view():
-    return render_template("base.html")
+    return render_template("index.html")
 
 
 @app.get("/about/", endpoint="about")
 def about_page():
-    return """One day the Hare laughed at the short feet and slow speed of the Tortoise. The Tortoise replied: "You may be as fast as the wind, but I will beat you in a race!" The Hare thought this idea was impossible and he agreed to the proposal. It was agreed that the Fox should choose the course and decide the end. The day for the race came, and the Tortoise and Hare started together. The Tortoise never stopped for a moment, walking slowly but steadily, right to the end of the course. The Hare ran fast and stopped to lie down for a rest. But he fell fast asleep. Eventually, he woke up and ran as fast as he could. But when he reached the end, he saw the Tortoise there already, sleeping comfortably after her effort."""
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
