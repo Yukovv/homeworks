1f2d7e63e349fbed725e95e99bd2bd87874b108b
user <JIK0009@yandex.ru>
2022-06-24 11:44:58 +0500

    create structure

---
+++ homework_06/blog-app/config.py
@@ -0,0 +1,31 @@
+from os import getenv
+
+SQLALCHEMY_DATABASE_URI = getenv(
+    "SQLALCHEMY_DATABASE_URI",
+    "postgresql+psycopg2://john:passwd!@127.0.0.1:5434/blog",
+)
+
+
+class Config:
+    DEBUG = False
+    TESTING = False
+    ENV = "development"
+
+    SECRET_KEY = "oiuweohfewbvkdsfkewrjrkwrnnqwertyasdfgsupersecret"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI
+
+
+class TestingConfig(Config):
+    TESTING = False
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    ENV = "production"
+    SECRET_KEY = "prodoiuwfkewrjrkwrnrtyasdfgsupersecret"
