1f77c62df1474af35195d017e9b3f928b47aaabf
user <JIK0009@yandex.ru>
2022-06-24 16:40:10 +0500

    add views for index, users_list and posts_list

--- homework_06/blog-app/app.py
+++ homework_06/blog-app/app.py
@@ -1,3 +1,31 @@
-from flask import Flask
+from os import getenv
+
+from flask import Flask, request, render_template, flash, url_for
+from flask_migrate import Migrate
+
+from models.database import db
+from models import User, Post
 
 app = Flask(__name__)
+config_name = "config.%s" % getenv("Config", "DevelopmentConfig")
+app.config.from_object(config_name)
+
+db.init_app(app)
+migrate = Migrate(app, db, compare_type=True)
+
+
+@app.route("/", endpoint="index")
+def index_page():
+    return render_template("index.html")
+
+
+@app.get("/users/", endpoint="users")
+def users_list():
+    users = User.query.all()
+    return render_template("list.html", elements_list=users, list_name="users")
+
+
+@app.get("/posts/", endpoint="posts")
+def posts_list():
+    posts = Post.query.all()
+    return render_template("list.html", elements_list=posts, list_name="posts")
