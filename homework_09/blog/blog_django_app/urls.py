8eb17e65420a7332b4967d759107bd53b6682cd6
user <JIK0009@yandex.ru>
2022-07-12 12:16:56 +0700

    copy homework_08 to homework_09

---
+++ homework_09/blog/blog_django_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .views import (
+    UserListView,
+    PostListView,
+    PostDetailsView,
+    UserDetailsView,
+)
+
+app_name = "blog_django_app"
+
+urlpatterns = [
+    path('users/', UserListView.as_view(), name="users"),
+    path('users/<int:pk>/', UserDetailsView.as_view(), name="user_details"),
+    path('posts/', PostListView.as_view(), name="posts"),
+    path('posts/<int:pk>/', PostDetailsView.as_view(), name="post_details"),
+]
